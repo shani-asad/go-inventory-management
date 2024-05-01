7d47acbc42128dcadc9706562c62a3668f032e1d
shani <mshaniasadulhaq@gmail.com>
2024-05-01 23:36:43 +0700

    feat: define all models

--- model/database/cat.go
+++ model/database/cat.go
@@ -3,8 +3,13 @@ package database
 import "time"
 
 type Cat struct {
-	Id        int
-	Name      string
-	CreatedAt time.Time
-	UpdatedAt time.Time
+	Id        	int
+	Name      	string
+	Race		string
+	Sex			string
+	AgeInMonth	int
+	Description	string // not null, minLength 1, maxLength 200
+	CreatedAt	time.Time
+	UpdatedAt	time.Time
+	ImageUrls	[]string // not null, minItems: 1, items: not null, should be url
 }
