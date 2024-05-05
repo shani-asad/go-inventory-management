f366478a2f26e0305a5a8dee01eabb5be8c52c51
Najatul Muslim Dinatra <najatulmuslimdinatra@gmail.com>
2024-05-05 14:20:10 +0700

    add log to each api handler

--- src/handler/match_handler.go
+++ src/handler/match_handler.go
@@ -4,7 +4,7 @@ import (
 	"cats-social/model/dto"
 	"cats-social/src/usecase"
 	"database/sql"
-	"fmt"
+	"log"
 	"net/http"
 	"strconv"
 
@@ -23,6 +23,7 @@ func (h *MatchHandler) CreateMatch(c *gin.Context) {
 	var request dto.RequestCreateMatch
 	err := c.ShouldBindJSON(&request)
 	if err != nil {
+		log.Println("create match bad request ", err)
 		c.JSON(400, gin.H{"status": "bad request", "message": err})
 		return
 	}
@@ -32,10 +33,12 @@ func (h *MatchHandler) CreateMatch(c *gin.Context) {
 
 	err = h.iMatchUsecase.CreateMatch(request)
 	if err != nil {
+		log.Println("create match internal error ", err)
 		c.JSON(500, gin.H{"status": "internal server error", "message": err})
 		return
 	}
 
+	log.Println("create match success")
 	c.JSON(200, gin.H{
 		"message": "successfully send match request",
 	})
@@ -49,10 +52,12 @@ func (h *MatchHandler) GetMatch(c *gin.Context) {
 	userId, _  := c.Get("user_id")
 	response, err := h.iMatchUsecase.GetMatch(userId.(int))
 	if err != nil {
+		log.Println("get match internal error ", err)
 		c.JSON(500, gin.H{"status": "internal server error", "message": err})
 		return
 	}
 
+	log.Println("get match success")
 	c.JSON(200, gin.H{
 		"message": "successfully get match requests",
 		"data": response,
@@ -68,6 +73,7 @@ func (h *MatchHandler) DeleteMatch(c *gin.Context) {
 
 	err = h.iMatchUsecase.GetMatchById(id)
 	if err != nil {
+		log.Println("delete match not found ", err)
 		if err == sql.ErrNoRows {
 			c.JSON(http.StatusNotFound, gin.H{"error": "match not found"})
 			return
@@ -78,10 +84,12 @@ func (h *MatchHandler) DeleteMatch(c *gin.Context) {
 
 	err = h.iMatchUsecase.DeleteMatch(id)
 	if err != nil {
+		log.Println("delete match internal error ", err)
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to delete match from the database"})
 		return
 	}
 
+	log.Println("delete match success")
 	c.JSON(http.StatusOK, gin.H{"message": "success"})
 }
 
@@ -89,14 +97,14 @@ func (h *MatchHandler) ApproveMatch(c *gin.Context) {
 	var request dto.RequestApproveMatch
 	err := c.ShouldBindJSON(&request)
 	if err != nil {
-		fmt.Println(err)
+		log.Println("approve match bad request ", err)
 		c.JSON(400, gin.H{"status": "bad request", "message": err})
 		return
 	}
 
 	err = h.iMatchUsecase.GetMatchById(request.MatchId)
 	if err != nil {
-		fmt.Println(err)
+		log.Println("approve match not found ", err)
 		if err == sql.ErrNoRows {
 			c.JSON(http.StatusNotFound, gin.H{"error": "match not found"})
 			return
@@ -110,11 +118,12 @@ func (h *MatchHandler) ApproveMatch(c *gin.Context) {
 	err = h.iMatchUsecase.ApproveMatch(request.MatchId, matchCatId, userCatId)
 
 	if err != nil {
-		fmt.Println(err)
+		log.Println("approve match internal error ", err)
 		c.JSON(500, gin.H{"status": "internal server error", "message": err})
 		return
 	}
 
+	log.Println("approve match success")
 	c.JSON(200, gin.H{
 		"message": "successfully approve match request",
 	})
